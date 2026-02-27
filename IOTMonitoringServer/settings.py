27372e039912e68898f1bb5c98659edac5718ed8
YuelyArce <y.arcea@uniandes.edu.co>
2026-02-27 17:40:25 -0500

    IP de base de datos corregida a PostgresInstance v14

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "44.201.240.103",  # Dirección IP de la base de datos
+        "HOST": "18.204.209.179",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
